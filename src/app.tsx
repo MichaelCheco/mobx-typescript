443d78a0854b8b29126699f7d42e17f1bd06a3f1
Michael <checomichael2@gmail.com>
2019-03-13 14:38:09 -0400

    render form and items

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,35 @@ import * as ReactDOM from 'react-dom';
 import { observable, action, computed } from 'mobx';
 import { observer } from 'mobx-react';
 import MobxReactDevtools from 'mobx-react-devtools';
+import { appState } from './appState';
 
+@observer
 class Application extends React.Component {
 	render() {
-		return <div>Hello</div>;
+		return (
+			<div>
+				<form
+					onSubmit={e => {
+						e.preventDefault();
+						appState.addCurrentItem();
+					}}>
+					<input
+						type="text"
+						value={appState.currentItem}
+						onChange={e => appState.changeCurrentItem(e.target.value)}
+					/>
+					<button type="submit">Add</button>
+					<button type="button" onClick={() => appState.reset()}>
+						Reset
+					</button>
+				</form>
+				<ul>
+					{appState.items.map((item, i) => {
+						return <li key={i}>{item}</li>;
+					})}
+				</ul>
+			</div>
+		);
 	}
 }
 
